4527b303f60160a7500c68f53b3a21fa33b19dae
ToanNguyen <toannguyen200044@gmail.com>
2023-09-19 22:41:12 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    output: "standalone",
+    distDir: "build",
+  };
 
 module.exports = nextConfig;

79b9dd061b4d6a5e317a1ac5428fecefc699afec
Khánh Đan Nguyễn Nhật <dan.nhatkhanh@gmail.com>
2023-10-11 21:03:43 +0700

    optimize showing tooltip

--- src/components/ThemeMusic/thememusic.component.tsx
+++ src/components/ThemeMusic/thememusic.component.tsx
@@ -34,25 +34,31 @@ function ThemeMusic({ title, audio, top, left, right, bottom }: Props) {
         position: 'absolute',
       }}
     >
-      <div className="group flex flex-col justify-center items-center gap-2">
+      <div className="relative group flex flex-col justify-center items-center gap-2">
         <button
           onClick={togglePlayPause}
-          className="nan-tooltip relative border-4 h-10 w-10 rounded-full bg-transparent cursor-pointer z-20 hover:border-yellow-200 group"
+          className="relative border-4 h-10 w-10 rounded-full bg-transparent cursor-pointer z-20 hover:border-yellow-200 group"
         >
-          <div className="m-1 absolute top-0 left-0 right-0 bottom-0 rounded-full bg-white group-hover:bg-yellow-200 group-hover:flex justify-center items-center hidden"></div>
+          <div className="m-1 absolute z-20 top-0 left-0 right-0 bottom-0 rounded-full bg-white group-hover:bg-yellow-200 group-hover:flex justify-center items-center hidden"></div>
         </button>
 
         <div
           className={`${
-            isPlaying ? 'group-hover:opacity-100' : ''
-          } bg-black/20 rounded-lg w-40 px-3 py-1 opacity-0`}
+            isPlaying ? '-bottom-16' : '-bottom-10'
+          } absolute bg-black/20 rounded-lg w-40 px-3 py-1 group-hover:block hidden`}
         >
-          <p>{title}</p>
-          <AudioOnObject
-            play={isPlaying}
-            audioRef={audioRef}
-            audioURL={audio}
-          />
+          <p className="text-center">{title}</p>
+          <div
+            className={`${
+              isPlaying ? 'group-hover:opacity-100' : 'hidden'
+            } opacity-0`}
+          >
+            <AudioOnObject
+              play={isPlaying}
+              audioRef={audioRef}
+              audioURL={audio}
+            />
+          </div>
         </div>
       </div>
     </div>

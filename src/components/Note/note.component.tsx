337dc6567ebb3232d5a798d8bb9e0a2f88fb042e
Khánh Đan Nguyễn Nhật <dan.nhatkhanh@gmail.com>
2023-11-02 23:26:26 +0700

    fixed error display

--- src/components/Note/note.component.tsx
+++ src/components/Note/note.component.tsx
@@ -7,8 +7,8 @@ function NoteTaking() {
   const { isOpenNote } = useContext(NoteTakingContext);
   return isOpenNote ? (
     <div className="fixed bottom-0 left-0 right-0 top-0 z-10">
-      <div className="m-10 flex h-[80vh] gap-3 rounded-xl bg-white/20 p-5">
-        <div className="h-60 w-1/4 rounded-xl bg-red-300 p-3 text-blue-900">
+      <div className="grid-row-1 m-10 grid max-h-[80vh] grid-flow-row gap-3 overflow-auto rounded-xl bg-white/20 p-5 md:grid-cols-3">
+        <div className="rounded-xl bg-red-300 p-3 text-blue-900">
           <h2 className="text-xl font-bold">Example</h2>
           <p className="text-sm">Wednesday 1st, November,2023</p>
           <p className="pt-3 text-left">
@@ -18,7 +18,7 @@ function NoteTaking() {
             nostrum enim tenetur.
           </p>
         </div>
-        <div className="h-60 w-1/4 rounded-xl bg-orange-300 p-3 text-blue-900">
+        <div className="rounded-xl bg-orange-300 p-3 text-blue-900">
           <h2 className="text-xl font-bold">Example</h2>
           <p className="text-sm">Wednesday 1st, November,2023</p>
           <p className="pt-3 text-left">
@@ -28,10 +28,10 @@ function NoteTaking() {
             nostrum enim tenetur.
           </p>
         </div>
-        <div className="group flex h-60 w-1/4 cursor-pointer items-center justify-center rounded-xl bg-white p-3">
+        <div className="group flex w-auto cursor-pointer items-center justify-center rounded-xl bg-white p-3">
           <FontAwesomeIcon
             icon={faPlusCircle}
-            className="h-20 w-20 py-28 text-blue-900 transition-transform duration-300 group-hover:scale-110 group-hover:brightness-110"
+            className="h-12 w-12 px-20 py-28 text-blue-900 transition-transform duration-300 group-hover:scale-110 group-hover:brightness-110"
           />
         </div>
       </div>
